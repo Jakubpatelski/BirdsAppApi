b076a026e375b3fcc3436e57451721b21d09b46f
Jakubpatelski <jakubpatelski24@gmail.com>
2023-03-12 20:32:10 +0100

    Initial Commit

---
+++ BirdsAppApi/BirdsAppApiApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirdsAppApiApp.swift
+//  BirdsAppApi
+//
+//  Created by Jakub Patelski on 12/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct BirdsAppApiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
